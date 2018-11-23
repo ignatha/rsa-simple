fcad13d4d096fc7b7c22919c3d2bcd9a23601e4c
Ignatha Prabowo <prabowoid.2@gmail.com>
2018-11-24 06:31:58 +0700

    Optimzed

--- rsa.py
+++ rsa.py
@@ -1,8 +1,9 @@
 from __future__ import division
 import Gcd
+import timeit
 
-class Rsa:
-   'Rsa Kriptografi'
+class rsa:
+   'rsa_bowo Kriptografi'
    n,m,e,d=0,0,0,0
 
    def __init__(self, p, q):
@@ -10,19 +11,16 @@ class Rsa:
       self.q = q
   
   # set Public key N
-
    def setN(self):
       self.n=self.p*self.q
 
    # Set m
-
    def setM(self):
       self.m=(self.p-1)*(self.q-1)
 
    # set Public key E
-
    def setE(self):
-      for x in range(500,self.m):
+      for x in range(99999,80000,-1):
          c = Gcd.computeGCD(x,self.m)
 
          if c == 1:
@@ -30,7 +28,6 @@ class Rsa:
             break
 
    # set Private Key
-
    def setD(self):
       i,o = 1,False
 
@@ -42,51 +39,77 @@ class Rsa:
       self.d = int(t)
 
    # Do Encrypt
-
    def doEncrypt(self,p):
-      c = p ** self.e % self.n
+      c = pow(p,self.e,self.n)
       return c 
 
    # Do Decrypt
-
    def doDecrypt(self,c):
-      p = c ** self.d % self.n
+      p = pow(c,self.d,self.n)
       return p
          
       
       
 
 
-rsa = Rsa(983, 991)
-rsa.setN()
-rsa.setM()
-rsa.setE()
-rsa.setD()
-print(rsa.n)
-print(rsa.m)
-print(rsa.e)
-print(rsa.d)
+rsa_bowo = rsa(857, 991)
+rsa_bowo.setN()
+rsa_bowo.setM()
+rsa_bowo.setE()
+rsa_bowo.setD()
 
-chiperText = rsa.doEncrypt(88)
-print(chiperText)
+# print("Nilai P : ")
+# print(rsa_bowo.p)
 
-plainText = rsa.doDecrypt(chiperText)
+# print("Nilai Q : ")
+# print(rsa_bowo.q)
+
+# print("Nilai N : ")
+# print(rsa_bowo.n)
+
+# print("Nilai M : ")
+# print(rsa_bowo.m)
+
+# print("Nilai E : ")
+# print(rsa_bowo.e)
+
+# print("Nilai D : ")
+# print(rsa_bowo.d)
+
+print("Plaintext : ")
+text = "Python adalah bahasa pemrograman interpretatif multiguna[9] dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.[10] Python diklaim sebagai bahasa yang menggabungkan kapabilitas, kemampuan, dengan sintaksis kode yang sangat jelas,[11] dan dilengkapi dengan fungsionalitas pustaka standar yang besar serta komprehensif. Python juga didukung oleh komunitas yang besar. Python mendukung multi paradigma pemrograman, utamanya; namun tidak dibatasi; pada pemrograman berorientasi objek, pemrograman imperatif, dan pemrograman fungsional. Salah satu fitur yang tersedia pada python adalah sebagai bahasa pemrograman dinamis yang dilengkapi dengan manajemen memori otomatis. Seperti halnya pada bahasa pemrograman dinamis lainnya, python umumnya digunakan sebagai bahasa skrip meski pada praktiknya penggunaan bahasa ini lebih luas mencakup konteks pemanfaatan yang umumnya tidak dilakukan dengan menggunakan bahasa skrip. Python dapat digunakan untuk berbagai keperluan pengembangan perangkat lunak dan dapat berjalan di berbagai platform sistem operasi."
+print(text)
+print("\n")
+
+start_plain = timeit.default_timer()
+plainText = [int(ord(c)) for c in text]
+print("ASCII Plaintext : ")
 print(plainText)
+print("\n")
+print("Need Time : ",timeit.default_timer() - start_plain)
+print("\n")
 
-print("---------------")
+start_encr = timeit.default_timer()
+cipherText = [int(rsa_bowo.doEncrypt(s)) for s in plainText]
+print("Encrypted : ")
+print(cipherText)
+print("\n")
+print("Need Time : ",timeit.default_timer() - start_encr)
+print("\n")
 
-rsa2 = Rsa(311, 101)
-rsa2.setN()
-rsa2.setM()
-rsa2.setE()
-rsa2.setD()
-print(rsa2.n)
-print(rsa2.m)
-print(rsa2.e)
-print(rsa2.d)
+start_decr = timeit.default_timer()
+textAsli = [int(rsa_bowo.doDecrypt(t)) for t in cipherText]
+print("Decrypted : ")
+print(textAsli)
+print("\n")
+print("Need Time : ",timeit.default_timer() - start_decr)
+print("\n")
 
-chiperText2 = rsa2.doEncrypt(20)
-print(chiperText2)
+start_asli = timeit.default_timer()
+Teks = [chr(t) for t in textAsli]
+print("Text : ")
+print(Teks)
+print("\n")
+print("Need Time : ",timeit.default_timer() - start_asli)
+print("\n")
 
-plainText2 = rsa2.doDecrypt(chiperText2)
-print(plainText2)
