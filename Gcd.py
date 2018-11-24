f40b940f4ef512e166336e1f23777fbbd0feec29
Ignatha Prabowo <prabowoid.2@gmail.com>
2018-11-24 22:52:17 +0700

    Rsa Flask

--- Gcd.py
+++ Gcd.py
@@ -1,12 +1,6 @@
 # GCD operator
-def computeGCD(x, y): 
-     
-   if x > y: 
-      small = y 
-   else: 
-      small = x 
-      for i in range(1, small+1): 
-         if((x % i == 0) and (y % i == 0)): 
-            gcd = i 
-
-      return gcd
+def computeGCD(a,b):
+	if(b==0):
+		return a
+	else:
+		return computeGCD(b,a%b)
--- rsa.py
+++ rsa.py
@@ -1,6 +1,7 @@
 from __future__ import division
 import Gcd
 import timeit
+import random
 
 class rsa:
    'rsa_bowo Kriptografi'
@@ -20,12 +21,13 @@ class rsa:
 
    # set Public key E
    def setE(self):
-      for x in range(99999,80000,-1):
+      list = []
+      for x in range(10000,99999):
          c = Gcd.computeGCD(x,self.m)
-
          if c == 1:
-            self.e = x
-            break
+            list.append(x)
+
+      self.e = random.choice(list)
 
    # set Private Key
    def setD(self):
@@ -52,11 +54,11 @@ class rsa:
       
 
 
-rsa_bowo = rsa(857, 991)
-rsa_bowo.setN()
-rsa_bowo.setM()
-rsa_bowo.setE()
-rsa_bowo.setD()
+# rsa_bowo = rsa(857, 991)
+# rsa_bowo.setN()
+# rsa_bowo.setM()
+# rsa_bowo.setE()
+# rsa_bowo.setD()
 
 # print("Nilai P : ")
 # print(rsa_bowo.p)
@@ -76,40 +78,40 @@ rsa_bowo.setD()
 # print("Nilai D : ")
 # print(rsa_bowo.d)
 
-print("Plaintext : ")
-text = "Python adalah bahasa pemrograman interpretatif multiguna[9] dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.[10] Python diklaim sebagai bahasa yang menggabungkan kapabilitas, kemampuan, dengan sintaksis kode yang sangat jelas,[11] dan dilengkapi dengan fungsionalitas pustaka standar yang besar serta komprehensif. Python juga didukung oleh komunitas yang besar. Python mendukung multi paradigma pemrograman, utamanya; namun tidak dibatasi; pada pemrograman berorientasi objek, pemrograman imperatif, dan pemrograman fungsional. Salah satu fitur yang tersedia pada python adalah sebagai bahasa pemrograman dinamis yang dilengkapi dengan manajemen memori otomatis. Seperti halnya pada bahasa pemrograman dinamis lainnya, python umumnya digunakan sebagai bahasa skrip meski pada praktiknya penggunaan bahasa ini lebih luas mencakup konteks pemanfaatan yang umumnya tidak dilakukan dengan menggunakan bahasa skrip. Python dapat digunakan untuk berbagai keperluan pengembangan perangkat lunak dan dapat berjalan di berbagai platform sistem operasi."
-print(text)
-print("\n")
+# print("Plaintext : ")
+# text = "Python adalah bahasa pemrograman interpretatif multiguna[9] dengan filosofi perancangan yang berfokus pada tingkat keterbacaan kode.[10] Python diklaim sebagai bahasa yang menggabungkan kapabilitas, kemampuan, dengan sintaksis kode yang sangat jelas,[11] dan dilengkapi dengan fungsionalitas pustaka standar yang besar serta komprehensif. Python juga didukung oleh komunitas yang besar. Python mendukung multi paradigma pemrograman, utamanya; namun tidak dibatasi; pada pemrograman berorientasi objek, pemrograman imperatif, dan pemrograman fungsional. Salah satu fitur yang tersedia pada python adalah sebagai bahasa pemrograman dinamis yang dilengkapi dengan manajemen memori otomatis. Seperti halnya pada bahasa pemrograman dinamis lainnya, python umumnya digunakan sebagai bahasa skrip meski pada praktiknya penggunaan bahasa ini lebih luas mencakup konteks pemanfaatan yang umumnya tidak dilakukan dengan menggunakan bahasa skrip. Python dapat digunakan untuk berbagai keperluan pengembangan perangkat lunak dan dapat berjalan di berbagai platform sistem operasi."
+# print(text)
+# print("\n")
 
-start_plain = timeit.default_timer()
-plainText = [int(ord(c)) for c in text]
-print("ASCII Plaintext : ")
-print(plainText)
-print("\n")
-print("Need Time : ",timeit.default_timer() - start_plain)
-print("\n")
+# start_plain = timeit.default_timer()
+# plainText = [int(ord(c)) for c in text]
+# print("ASCII Plaintext : ")
+# print(plainText)
+# print("\n")
+# print("Need Time : ",timeit.default_timer() - start_plain)
+# print("\n")
 
-start_encr = timeit.default_timer()
-cipherText = [int(rsa_bowo.doEncrypt(s)) for s in plainText]
-print("Encrypted : ")
-print(cipherText)
-print("\n")
-print("Need Time : ",timeit.default_timer() - start_encr)
-print("\n")
+# start_encr = timeit.default_timer()
+# cipherText = [int(rsa_bowo.doEncrypt(s)) for s in plainText]
+# print("Encrypted : ")
+# print(cipherText)
+# print("\n")
+# print("Need Time : ",timeit.default_timer() - start_encr)
+# print("\n")
 
-start_decr = timeit.default_timer()
-textAsli = [int(rsa_bowo.doDecrypt(t)) for t in cipherText]
-print("Decrypted : ")
-print(textAsli)
-print("\n")
-print("Need Time : ",timeit.default_timer() - start_decr)
-print("\n")
+# start_decr = timeit.default_timer()
+# textAsli = [int(rsa_bowo.doDecrypt(t)) for t in cipherText]
+# print("Decrypted : ")
+# print(textAsli)
+# print("\n")
+# print("Need Time : ",timeit.default_timer() - start_decr)
+# print("\n")
 
-start_asli = timeit.default_timer()
-Teks = [chr(t) for t in textAsli]
-print("Text : ")
-print(Teks)
-print("\n")
-print("Need Time : ",timeit.default_timer() - start_asli)
-print("\n")
+# start_asli = timeit.default_timer()
+# Teks = [chr(t) for t in textAsli]
+# print("Text : ")
+# print(Teks)
+# print("\n")
+# print("Need Time : ",timeit.default_timer() - start_asli)
+# print("\n")
 
